6612cabe56fd9c88592c8f6c3a212a5bcf37d6b8
alysson0321 <alyssonfdbr874@gmail.com>
2025-10-25 16:26:38 -0300

    conecta front com back

--- paroquia-frontend/app.js
+++ paroquia-frontend/app.js
@@ -1,6 +1,6 @@
 // <-- MUDANÇA AQUI: Defina a URL do seu backend (Web Service) do Render
 // Esta URL será algo como: https://paroquia-backend.onrender.com
-const API_URL = 'https://SEU_BACKEND_URL.onrender.com';
+const API_URL = 'https://paroquia-backend.onrender.com/';
 
 
 // menu lateral da pag incial 
